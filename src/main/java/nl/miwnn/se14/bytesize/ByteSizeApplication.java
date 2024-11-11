00e16c987c118a22930801a464a1c76a80c5b17a
Heron Meijering <heronmeijering@gmail.com>
2024-11-11 15:49:34 +0100

    Added Spring Initializer
    Added files from Spring Initializer

---
+++ src/main/java/nl/miwnn/se14/bytesize/ByteSizeApplication.java
@@ -0,0 +1,13 @@
+package nl.miwnn.se14.bytesize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteSizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByteSizeApplication.class, args);
+	}
+
+}
