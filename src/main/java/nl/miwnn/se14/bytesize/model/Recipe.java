da12aeb3ef91d782ffe787e0ce63552f6c673899
Ysrz <115709777+Ysrz@users.noreply.github.com>
2024-11-12 11:04:53 +0100

    push commit

--- src/main/java/nl/miwnn/se14/bytesize/model/Recipe.java
+++ src/main/java/nl/miwnn/se14/bytesize/model/Recipe.java
@@ -1,7 +1,57 @@
 package nl.miwnn.se14.bytesize.model;
 
+import jakarta.persistence.*;
+
 /**
- * @author Yvonne
+ * @author Yvonne.
+ * A recipe can be made and read by the user.
  */
+
+@Entity
 public class Recipe {
+    @Id @GeneratedValue
+    private Long recipeId;
+
+//    @NotBlank
+    @Column(unique = true)
+    private String title;
+
+    private String description;
+
+    private String imageUrl;
+
+//    @ManyToOne
+//    private User user;
+
+    public Long getRecipeId() {
+        return recipeId;
+    }
+
+    public void setRecipeId(Long recipeId) {
+        this.recipeId = recipeId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }
