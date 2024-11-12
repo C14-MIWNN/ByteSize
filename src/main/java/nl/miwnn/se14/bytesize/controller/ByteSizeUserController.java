e62b69c77e000f93d2d4e90fb88aee9058e1fbb6
Heron Meijering <heronmeijering@gmail.com>
2024-11-12 14:05:23 +0100

    Added a user overview

--- src/main/java/nl/miwnn/se14/bytesize/controller/ByteSizeUserController.java
+++ src/main/java/nl/miwnn/se14/bytesize/controller/ByteSizeUserController.java
@@ -1,6 +1,9 @@
 package nl.miwnn.se14.bytesize.controller;
 
+import nl.miwnn.se14.bytesize.service.ByteSizeUserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -11,4 +14,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/user")
 public class ByteSizeUserController {
+    private final ByteSizeUserService byteSizeUserService;
+
+    public ByteSizeUserController(ByteSizeUserService byteSizeUserService) {
+        this.byteSizeUserService = byteSizeUserService;
+    }
+
+    @GetMapping("/overview")
+    public String showUserOverview(Model datamodel) {
+        datamodel.addAttribute("allUsers", byteSizeUserService.getAllUsers());
+
+        return "userOverview";
+    }
+
 }
--- src/main/java/nl/miwnn/se14/bytesize/service/ByteSizeUserService.java
+++ src/main/java/nl/miwnn/se14/bytesize/service/ByteSizeUserService.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -39,5 +40,9 @@ public class ByteSizeUserService implements UserDetailsService {
     public void save(ByteSizeUser user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         byteSizeUserRepository.save(user);
+    }
+
+    public List<ByteSizeUser> getAllUsers() {
+        return byteSizeUserRepository.findAll();
     }
 }
