fc3b8485de5eea7c6a8b60a9255bb5bd520bfae1
Heron Meijering <heronmeijering@gmail.com>
2024-11-20 14:56:50 +0100

    Updated the recipe control so it loads pages by ID rather than title, and added a landingpage

--- src/main/java/nl/miwnn/se14/bytesize/repositories/RecipeRepository.java
+++ src/main/java/nl/miwnn/se14/bytesize/repositories/RecipeRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
     Optional<Recipe> findByRecipeTitle(String recipeTitle);
+    Optional<Recipe> findByRecipeId(Long recipeId);
 }
