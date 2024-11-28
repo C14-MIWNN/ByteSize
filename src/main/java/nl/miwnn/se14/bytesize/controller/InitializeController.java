3e65f58442967b3d673c716841c17d88dd01edb5
Heron Meijering <heronmeijering@gmail.com>
2024-11-28 10:14:08 +0100

    Added a created by section to the recipe details, and updated the initializer to connect a recipe creator to recipes

--- src/main/java/nl/miwnn/se14/bytesize/controller/InitializeController.java
+++ src/main/java/nl/miwnn/se14/bytesize/controller/InitializeController.java
@@ -35,42 +35,46 @@ public class InitializeController {
     }
 
     private void initializeDB() {
-        makeByteSizeUser("Admin", "AdminPassword", "ADMIN");
-        makeByteSizeUser("Heron", "123456", "USER");
+        ByteSizeUser admin = makeByteSizeUser("Admin", "AdminPassword", "ADMIN");
+        ByteSizeUser heron = makeByteSizeUser("Heron", "123456", "USER");
+        ByteSizeUser yvonne = makeByteSizeUser("Yvonne", "123456", "USER");
+        ByteSizeUser jantje = makeByteSizeUser("Jantje", "123456", "USER");
+        ByteSizeUser pietje = makeByteSizeUser("Pietje", "123456", "USER");
 
         Recipe indianChickenCurry = makeRecipe(
                 "Indian Chicken Curry",
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas at odio nec sem ultrices cursus.",
                 "https://cdn.pixabay.com/photo/2022/06/07/20/52/curry-7249247_1280.jpg",
-                "ingredients");
+                "ingredients", admin);
         Recipe beefSteak = makeRecipe(
                 "Beef Steak",
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas at odio nec sem ultrices cursus.",
                 "https://cdn.pixabay.com/photo/2023/03/21/16/55/beef-7867701_1280.jpg",
-                "ingredients");
+                "ingredients", heron);
         Recipe chickenLiverWrappedInBacon = makeRecipe(
                 "Chicken Liver Wrapped In Bacon",
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas at odio nec sem ultrices cursus.",
                 "https://cdn.pixabay.com/photo/2019/04/20/12/08/chicken-liver-4141673_1280.jpg",
-                "ingredients");
+                "ingredients", yvonne);
         Recipe fruitSaladWithYoghurt = makeRecipe(
                 "Fruit Salad With Yoghurt",
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas at odio nec sem ultrices cursus.",
                 "https://cdn.pixabay.com/photo/2019/01/12/16/21/breakfast-3928800_1280.jpg",
-                "ingredients");
+                "ingredients", jantje);
         Recipe bestCheesecakeEver = makeRecipe(
                 "Best Cheesecake Ever",
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas at odio nec sem ultrices cursus.",
                 "https://cdn.pixabay.com/photo/2019/07/21/19/52/cheesecake-4353495_1280.jpg",
-                "ingredients");
+                "ingredients",pietje);
     }
 
-    private Recipe makeRecipe(String recipeTitle, String recipeDescription, String imageUrl, String ingredients) {
+    private Recipe makeRecipe(String recipeTitle, String recipeDescription, String imageUrl, String ingredients, ByteSizeUser recipeCreator) {
         Recipe recipe = new Recipe();
         recipe.setRecipeTitle(recipeTitle);
         recipe.setRecipeDescription(recipeDescription);
         recipe.setImageUrl(imageUrl);
         recipe.setIngredients(ingredients);
+        recipe.setRecipeCreator(recipeCreator);
 
         recipeRepository.save(recipe);
         return recipe;
